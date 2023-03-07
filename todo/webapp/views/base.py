dbcd2df5ba29bf9a857ac71f21bbcda176e4ce86
Azat Muzapbar <azamuzapbar@gmail.com>
2023-03-07 16:46:02 +0600

    Добавление IndexView

---
+++ todo/webapp/views/base.py
@@ -0,0 +1,15 @@
+from django.views.generic import TemplateView
+
+from webapp.models import Task
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
+
+
+
